515fe253563e7553b842d7705a0d3469ad5bb1f9
Aditya1or0 <adityapandit264@gmail.com>
2025-09-05 21:02:09 +0530

    fixed cors issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,15 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors();
+  // app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://ecommerce-project-lovat-theta.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
